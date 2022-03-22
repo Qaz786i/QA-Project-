2f41ee64e128af89e44922a2c718d8de06cbd9ea
Qasim <qasimharoon@outlook.com>
2022-03-22 11:31:08 +0000

    removed os

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{os.getenv('MYSQL_ROOT_PASSWORD')}@mysql/app-db" #'sqlite:///jobenrollment.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('MYSQL_ROOT_PASSWORD')}@mysql/app-db" #'sqlite:///jobenrollment.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv ('SECRET_KEY')
+app.config['SECRET_KEY'] = getenv ('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
